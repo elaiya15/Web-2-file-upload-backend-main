85f1b69beacfdcf971959957ab07ac7eacffda8c
elaiya15 <elaiyakutty143@gmail.com>
2022-12-04 03:01:43 +0530

    first commit

---
+++ connect.js
@@ -0,0 +1,13 @@
+const { MongoClient } = require("mongodb");
+
+module.exports = {
+  selectedDb: {},
+  async connect() {
+    try {
+      const client = await MongoClient.connect(process.env.MONGODB_URL);
+      this.selectedDb = client.db("guvi");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
